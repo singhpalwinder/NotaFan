37b453d6f9f06d2c7c1ca93c7fe39fa22fc9ede9
Paul Singh <palwinders969@gmail.com>
2024-09-25 17:58:02 -0500

    simplified code

--- notaFan.py
+++ notaFan.py
@@ -3,75 +3,56 @@ import instaloader
 from instaloader.exceptions import TwoFactorAuthRequiredException
 import credentials
 
-L = instaloader.Instaloader()
 
-# Login or load session
-
-userName = credentials.userName
-password = credentials.password
-auth_code=credentials.auth_code
-fileName1 = "notFollowingBack.txt"
-fileName2 = "updated.txt"
-
-try:
-    L.login(userName, password)
-except TwoFactorAuthRequiredException:
-     L.two_factor_login(auth_code)
-     L.save_session_to_file('loginSession')
-
-# Obtain profile metadata
-profile = instaloader.Profile.from_username(L.context, userName)
-
-# Print list of followees
-followers_list = [] #current followers list
-
-following_list = [] #current following list
-
-notFollowing_me = [] #complement of current following and followers list
-
-lastChecked_list = [] #list from txt file from last session
-
-updatedNotFollowingBack = [] #complement of notfollowing_me and lastChecked_list
-
-
-def getNotFollowingBack():
-    for followers in profile.get_followers(): # get list of people that currently follow me
-        followers_list.append(followers.username)
-   
-    for following in profile.get_followees(): # get list of people that I follow
-        following_list.append(following.username)
-   
-
-    for count in following_list:        #insert names of people that Currently dont follow you back in to notFollowing_me
-        if count not in followers_list:
-            notFollowing_me.append(count)
-        
-getNotFollowingBack() #get list of people currrently not following back 
-
-while True:
+def readData(fileName):
     try:
-        with open(fileName1, 'r') as f: #get names from last session if file exists
+        with open(fileName, 'r') as f: #get names from last session if file exists
             lastChecked_list = f.read().splitlines()
-            break
+            return lastChecked_list
     except:
         print("You are running this script for the first time therefore file DNE...Creating file\n")        #if files dont exist create them and tell user how they will be updated
-        print("Created file " + fileName1 + " (updated with people not following you back right now).\n")
-        print("Created file " + fileName2 + ", if you run this script again this file will be updated with new users not following you back\n")
-        with open(fileName1, 'w') as fp:
-            for items in notFollowing_me:
-                fp.write("%s\n" % items)
+        print("Created file " + fileName + " (updated with people not following you back right now).\n")
+        with open(fileName, 'w') as fp:
+                fp.write("")
+def writeData(fileName, notFollowingBack):
 
-  
-with open(fileName2, 'w') as fp:
-    for items in notFollowing_me:
-        if items not in lastChecked_list: #write complement of notFollowing_me and lastChecked_list to updated_notFollowing_Back.txt
-            updatedNotFollowingBack.append(items)
-            fp.write("%s\n" % items)
+    # add new names to first file 
+    with open(fileName, 'a') as f:
+        for items in notFollowingBack:
+            f.write("%s\n" % items)
+def getNotFollowingBack(user, password, auth, lastChecked):
+    L = instaloader.Instaloader()
 
-with open(fileName1, 'a') as f:
-    for items in updatedNotFollowingBack:
-        f.write("%s\n" % items)
+    try:
+        L.login(user, password)
+    except TwoFactorAuthRequiredException:
+        L.two_factor_login(auth)
+        L.save_session_to_file('loginSession')
 
+    # Obtain profile metadata
+    profile = instaloader.Profile.from_username(L.context, user)
 
+    # Get lists of followers and following
+    followers_list = {follower.username for follower in profile.get_followers()}
+    following_list = {followee.username for followee in profile.get_followees()}
 
-print('Done')
+    # Find people not following back and not in lastChecked
+    return [person for person in following_list if person not in followers_list and person not in lastChecked]
+def main():
+    userName = credentials.userName
+    password = credentials.password
+    auth_code= int('123691')
+    file = "notFollowingBack.txt"
+
+    # load old data
+    old_list = readData(file)
+
+    updated_notFollowingBack = getNotFollowingBack(userName, password, auth_code, old_list)
+
+    # save new data to file
+    writeData(file, updated_notFollowingBack)
+    print(updated_notFollowingBack)
+    print('Done')
+
+if __name__ == "__main__":
+    main()
